9d35881204a49a9ac49c58b141e2a291c8bc6a1b
Marcelo Cohen <mflashbr@gmail.com>
2022-08-30 20:40:13 -0300

    Animação do jogador com AnimatedSprite (mais simples)

--- PlayerControl.gd
+++ PlayerControl.gd
@@ -6,10 +6,12 @@ export var rotation_speed = 1.5
 var velocity = Vector2()
 var rotation_dir = 0 # ang. inicial de rotação
 
-onready var target = position # alvo inicial é a própria posição
+onready var target := position # alvo inicial é a própria posição
+onready var player := $Player
 
 func _ready() -> void:
-	$Player.rotation_degrees = 0
+	player.rotation_degrees = 0
+	player.stop()
 	#target = position
 	
 # Usada apenas para o modo de point-and-click
@@ -40,15 +42,28 @@ func get_8way_input():
 	velocity.x = Input.get_action_strength("right")-Input.get_action_strength("left")
 	velocity.y = Input.get_action_strength("down")-Input.get_action_strength("up")
 	velocity = velocity.normalized() * speed
+	if velocity.x < 0:
+		player.play("walk-left")
+	elif velocity.x > 0:
+		player.play("walk-right")
+	elif velocity.y > 0:
+		player.play("walk-down")
+	elif velocity.y < 0:
+		player.play("walk-up")
+	else:
+		#player.animation = "walk-down"
+		player.frame = 0
+		player.stop()
+		
 
 func calc_velocity_click():
 	velocity = position.direction_to(target) * speed
 	
 func _physics_process(delta):
-	#get_8way_input()
+	get_8way_input()
 	#get_rotate_input()
-	get_rotate_mouse_input()
+	#get_rotate_mouse_input()
 	#calc_velocity_click()
-	rotation += rotation_dir * rotation_speed * delta
+	#rotation += rotation_dir * rotation_speed * delta
 	#if position.distance_to(target) > 5:
 	velocity = move_and_slide(velocity)
